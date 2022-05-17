e8f899b5743fa237e85a0aa7ac6abb005bc4a8b1
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-05-17 14:21:45 -0300

    Propriedades são somente leitura

--- exercicios/src/components/Aleatorio/Aleatorio.js
+++ exercicios/src/components/Aleatorio/Aleatorio.js
@@ -5,6 +5,8 @@ import Estilo from '../Estilo/estilo';
 
 export default ( { min, max } ) => {
     // const { min, max } = props;
+    min += 1000;
+    max += 2000;
     const numero = Math.floor(Math.random() * max + min);
     return (
         <Text style={Estilo.fontG}>Nº Aleatório: {numero} </Text>
