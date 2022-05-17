2e573f7cb225a9c3946b8ce30fead850079dfe66
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-05-17 10:19:55 -0300

    SafeAreaView

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,1 +1,12 @@
-import { SafeAreaView } from "react-native";
+import React from 'react';
+import { Text, SafeAreaView, View } from 'react-native';
+
+export default () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Sem Título</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
