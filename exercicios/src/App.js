c25ad44781869706efe43e26ec17455b65852d6b
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-05-17 10:26:58 -0300

    Primeiro componente #01

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import { Text, SafeAreaView, View } from 'react-native';
+// import React from 'react';
+// import { Text, SafeAreaView, View } from 'react-native';
 
-export default () => {
-    return (
-        <SafeAreaView>
-            <View>
-                <Text>Sem Título</Text>
-            </View>
-        </SafeAreaView>
-    );
+// export default () => {
+//     return (
+//         <SafeAreaView>
+//             <View>
+//                 <Text>Sem Título</Text>
+//             </View>
+//         </SafeAreaView>
+//     );
+// }
+
+import React from "react";
+import { Text } from 'react-native';
+
+
+function App() {
+    const jsx = <Text>Primeiro Componente</Text>;
+    return jsx;
 }
+
+export default App;
