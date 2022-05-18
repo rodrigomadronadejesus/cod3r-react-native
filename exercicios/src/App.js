fc9dd0a5c41f57e084e9a36f72bd13337151e3a0
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-05-18 15:15:08 -0300

    Usando flexbox #03

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -45,7 +45,8 @@
 import React from 'react';
 import { View, SafeAreaView, StyleSheet } from 'react-native';
 
-import FlexboxV2 from './components/layout/FlexboxV2';
+import FlexboxV3 from './components/layout/FlexboxV3';
+// import FlexboxV2 from './components/layout/FlexboxV2';
 // import FlexboxV1 from './components/layout/FlexboxV1';
 // import Quadrado from './components/layout/Quadrado';
 // import DigiteSeuNome from './components/DigiteSeuNome/DigiteSeuNome';
@@ -71,8 +72,9 @@ export default () => {
     return (
         <SafeAreaView style={style.App}> 
             <View>
-                <FlexboxV2/>
+                <FlexboxV3/>
                 {/*
+                    <FlexboxV2/>
                     <FlexboxV1/>
                     <DigiteSeuNome/>
                     <ListaProdutosV2/>
