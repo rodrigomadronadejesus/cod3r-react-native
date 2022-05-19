d65bc0352130ada4433437318bdda4b231573d48
Rodrigo Madrona De Jesus <rodrigomadronadejesus@outlook.com>
2022-05-19 17:53:19 -0300

    Parâmetros do jogo

---
+++ mines/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from 'react-native';
+import params from './Params';
+
+export default class App extends Component {
+
+
+    render (){
+        return (
+            <View>
+                <Text>Iniciando o Mines!</Text>
+                <Text>Tamanho da grade: </Text>
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create ({
+    
+});
