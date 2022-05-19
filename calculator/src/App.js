fc3a143b6d0a33629c49a6991e22c05dbf6ddcb8
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-05-19 09:51:23 -0300

    Componente botão #01

---
+++ calculator/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+
+import Button from './components/Button';
+
+export default class App extends Component {
+    render (){
+
+        return (
+            <View style={styles.Container}>
+                <View style={styles.Buttons}>
+                    <Button label='AC'/>
+                    <Button label='/'/>
+                    <Button label='7'/>
+                    <Button label='8'/>
+                    <Button label='9'/>
+                    <Button label='*'/>
+                    <Button label='4'/>
+                    <Button label='5'/>
+                    <Button label='6'/>
+                    <Button label='-'/>
+                    <Button label='1'/>
+                    <Button label='2'/>
+                    <Button label='3'/>
+                    <Button label='+'/>
+                    <Button label='0'/>
+                    <Button label='.'/>
+                    <Button label='='/>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    Container: {
+        flex: 1
+    },
+    Buttons: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    }
+});
