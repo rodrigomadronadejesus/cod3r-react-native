d4e28e487b06dbdd7475084e1a0bcbbce1248af9
Rodrigo Madrona De Jesus <rodrigomadronadejesus@outlook.com>
2022-05-25 18:09:37 -0300

    Lista de usuários #01

--- rncrud/src/data/users.js
+++ rncrud/src/data/users.js
@@ -1,26 +1,26 @@
 export default [
     {
         id: 1,
-        nome: 'João Silva',
-        email: 'josil@empmaill.com',
+        name: 'João Silva',
+        name: 'josil@empmaill.com',
         avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
     },
     {
         id: 2,
-        nome: 'Mariana Aguiar',
+        name: 'Mariana Aguiar',
         email: 'marianaaguiar@empmaill.com',
-        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
+        avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'
     },
     {
         id: 3,
-        nome: 'Julio Fragoso',
+        name: 'Julio Fragoso',
         email: 'JuliaFral@empmaill.com',
-        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png'
     },
     {
         id: 4,
-        nome: 'Rafael Monteiro',
+        name: 'Rafael Monteiro',
         email: 'rafaelmonteiro@empmaill.com',
-        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png'
     }
 ];
--- rncrud/src/views/UserList.js
+++ rncrud/src/views/UserList.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, FlatList } from 'react-native';
+import users from '../data/users';
+import { Avatar, ListItem } from 'react-native-elements';
 
 export default props => {
+
+    function getUserItem ({ item: info }){
+        return (
+            <ListItem key={info.id} bottomDivider onPress={() => props.navigation.navigate('UserForm')}>
+                <Avatar source={{uri: info.avatarUrl}}/>
+                <ListItem.Content>
+                    <ListItem.Title>{info.name}</ListItem.Title>
+                    <ListItem.Subtitle>{info.email}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        );
+    }
+
     return (
-        <Text>UserList</Text>
+        <View>
+            <FlatList
+                data={users}
+                keyExtractor={user => user.id.toString()}
+                renderItem={getUserItem}
+            />
+        </View>
     );
 }
