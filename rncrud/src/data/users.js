9a76f8b12d512552e1a4cbdab0ae98ed23ad99dd
Rodrigo Madrona De Jesus <rodrigomadronadejesus@outlook.com>
2022-05-25 17:29:46 -0300

    Criando arquivo de usuários

---
+++ rncrud/src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        nome: 'João Silva',
+        email: 'josil@empmaill.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
+    },
+    {
+        id: 2,
+        nome: 'Mariana Aguiar',
+        email: 'marianaaguiar@empmaill.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
+    },
+    {
+        id: 3,
+        nome: 'Julio Fragoso',
+        email: 'JuliaFral@empmaill.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
+    },
+    {
+        id: 4,
+        nome: 'Rafael Monteiro',
+        email: 'rafaelmonteiro@empmaill.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
+    }
+];
