b069709e63c14fa24d5d2251251b5f54a8e25cb4
Rodrigo Madrona De Jesus <rodrigomadronadejesus@outlook.com>
2022-05-25 10:12:31 -0300

    Navegação stack #02

--- navegacao/src/views/TelaA.js
+++ navegacao/src/views/TelaA.js
@@ -2,6 +2,7 @@ import React from "react";
 import TextoCentral from "../components/TextoCentral";
 
 export default props => {
+   props.navigation.navigate("TelaB");
     return (
         <TextoCentral corFundo='#e53935'>
             Tela A
