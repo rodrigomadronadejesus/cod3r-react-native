b06477282861d2cb96b15c62215d6b135d23af91
Rodrigo Madrona De Jesus <rodrigomadronadejesus@outlook.com>
2022-05-25 09:42:52 -0300

    Preparando app #02

---
+++ navegacao/src/views/TelaC.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import TextoCentral from '../components/TextoCentral';
+
+export default props => {
+    return (
+        <TextoCentral corFundo='#9932cd'>
+            Tela C
+        </TextoCentral>
+    );
+}
