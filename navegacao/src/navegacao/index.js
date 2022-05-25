5b60e33275396f912bff17af7cfbfef9bee6dbb8
Rodrigo Madrona De Jesus <rodrigomadronadejesus@outlook.com>
2022-05-25 13:52:18 -0300

    Adicionando ícones #01

--- navegacao/src/navegacao/index.js
+++ navegacao/src/navegacao/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import { SafeAreaView } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import Drawer from "./Drawer";
+
+
+// import Drawer from "./Drawer";
 // import Stack from "./Stack";
-// import Tab from './Tab';
+import Tab from './Tab';
 
 export default props => {
     return (
@@ -12,7 +14,7 @@ export default props => {
             flex: 1
         }}>
             <NavigationContainer>
-               <Drawer/>
+                <Tab/>
             </NavigationContainer>
         </SafeAreaView>
     );
