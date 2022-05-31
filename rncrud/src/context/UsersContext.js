7be880c6eab211b4ea98a8a2112e47b4b3ff34c7
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-05-31 10:50:11 -0300

    useReducer #01

--- rncrud/src/context/UsersContext.js
+++ rncrud/src/context/UsersContext.js
@@ -1,16 +1,22 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
 import users from '../data/users';
 
-const UsersContext = createContext({
 
-});
+const initialState = { users };
+const UsersContext = createContext({});
 
 export const UsersProvider = props => {
+
+    function reducer (state, action){
+        console.warn(action);
+        return state;
+    }
+
+    const [state, dispatch] = useReducer (reducer, initialState);
+
     return (
         <UsersContext.Provider value={{
-            state: {
-                users
-            }
+            state, dispatch
         }}>
             {props.children}
         </UsersContext.Provider>
