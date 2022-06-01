d1e619121018945d10a3c19df258efdc7ca33044
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-06-01 15:49:01 -0300

    Configuração Android: problema

--- tasks/src/App.js
+++ tasks/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import TaskList from './screens/TaskList';
 
 export default () => {
     return (
-        <SafeAreaView>
-            <TaskList/>
-        </SafeAreaView>
+        <GestureHandlerRootView style={{flex: 1}}>
+            <SafeAreaView style={{flex: 1}}>
+                <TaskList/>
+            </SafeAreaView>
+        </GestureHandlerRootView>
+
     );
 }
