7b49f4c2a158299c028249d264302361f930caea
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-06-01 10:13:20 -0300

    Componente lista de tarefas #04

---
+++ tasks/src/commonStyles.js
@@ -0,0 +1,6 @@
+export default {
+    FontFamily: 'Lato',
+    Colors: {
+        Secondary: '#FFF',
+    }
+}
--- tasks/src/screens/TaskList.js
+++ tasks/src/screens/TaskList.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text, ImageBackground, StyleSheet } from 'react-native';
-
-import todayImage from '../../assets/imgs/today.jpg';
-
 import moment from 'moment';
 import 'moment/locale/pt-br';
+
+
+import commonStyles from '../commonStyles';
+import todayImage from '../../assets/imgs/today.jpg';
 
 export default class TaskList extends Component {
     render (){
@@ -15,12 +16,14 @@ export default class TaskList extends Component {
             <View style={styles.Container}>
                 <ImageBackground style={styles.Background} source={todayImage}>
                     <View style={styles.TitleBar}>
-                        <Text>Hoje</Text>
-                        <Text>{today}</Text>
+                        <Text style={styles.Title}>Hoje</Text>
+                        <Text style={styles.Subtitle}>{today}</Text>
                     </View>
                 </ImageBackground>
                 <View style={styles.TaskList}>
-                    <Text>TaskList</Text>
+                    <Text>Tarefa #01</Text>
+                    <Text>Tarefa #02</Text>
+                    <Text>Tarefa #03</Text>
                 </View>
             </View>
         );
@@ -40,5 +43,19 @@ const styles = StyleSheet.create({
     TitleBar: {
         flex: 1,
         justifyContent: 'flex-end'
+    },
+    Title: {
+        fontFamily: commonStyles.FontFamily,
+        color: commonStyles.Colors.Secondary,
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 20
+    },
+    Subtitle: {
+        fontFamily: commonStyles.FontFamily,
+        color: commonStyles.Colors.Secondary,
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30
     }
 });
