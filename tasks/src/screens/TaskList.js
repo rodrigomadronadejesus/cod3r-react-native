5ca95d26a550495d9e7a88d743d594fcbfac758b
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-06-01 09:49:55 -0300

    Componente lista de tarefas #02

--- tasks/src/screens/TaskList.js
+++ tasks/src/screens/TaskList.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+
+import todayImage from '../../assets/imgs/today.jpg';
 
 export default class TaskList extends Component {
     render (){
         return (
-            <View>
-                <Text>TaskList</Text>
+            <View style={styles.Container}>
+                <ImageBackground style={styles.Background} source={todayImage}/>
+                <View style={styles.TaskList}>
+                    <Text>TaskList</Text>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    Container: {
+        flexGrow: 1
+    },
+    Background: {
+        flexGrow: 3
+    },
+    TaskList: {
+        flexGrow: 7
+    }
+});
