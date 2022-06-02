b2308f154e6f426a145b5956b353174b63475bd9
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-06-02 09:33:00 -0300

    Middlewares & DB com Knex #01

--- tasks-backend/src/App.js
+++ tasks-backend/src/App.js
@@ -1,8 +1,10 @@
-const express = require ('express');
+const express = require('express');
+const knex = require('knex');
 const app = express();
 const app_port = 3000;
 
 app.get("/", (request, response) => {
+    console.log("func 1");
     response.status(200).send("Meu backend!");
 });
 
