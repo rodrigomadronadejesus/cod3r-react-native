3789b3b4f77d0c85dbb26655c34d40980e8be1f0
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-06-02 10:19:58 -0300

    Middlewares & DB com Knex #02

--- tasks-backend/src/App.js
+++ tasks-backend/src/App.js
@@ -1,7 +1,21 @@
+// Importações externas
 const express = require('express');
 const knex = require('knex');
+const consign = require('consign');
+
+// Importações internas
+const db = require('../config/db');
+
+// Aplicação
 const app = express();
 const app_port = 3000;
+
+
+consign()
+    .then('./config/middlewares.js')
+    .into(app);
+
+app.db = db;
 
 app.get("/", (request, response) => {
     console.log("func 1");
