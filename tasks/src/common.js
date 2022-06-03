0877646c210b53f9f5a41e62a27d4701584343c5
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-06-03 15:17:31 -0300

    Melhorando mensagens

--- tasks/src/common.js
+++ tasks/src/common.js
@@ -4,11 +4,21 @@ import { Alert, Platform } from 'react-native';
 const server = Platform.OS === 'ios' ? 'http://127.0.0.1:3000' : 'http://192.168.254.146:3000';
 
 function showError (err){
-    Alert.alert('Ops! Ocorreu um problema!', `Mensagem: ${err}`)
+    if(err.response && err.response.data){
+        Alert.alert('Ops! Ocorreu um problema!', `Mensagem: ${err.response.data}`);
+    }
+    else {
+        Alert.alert('Ops! Ocorreu um problema!', `Mensagem: ${err}`);
+    }
 }
 
 function showSuccess(msg){
-    Alert.alert('Sucesso!', msg);
+    if(msg.response && msg.response.data){
+        Alert.alert('Sucesso!', msg.response.data);
+    }
+    else {
+        Alert.alert('Ops! Ocorreu um problema!', `Mensagem: ${msg}`);
+    }
 }
 
 export { server, showError, showSuccess};
