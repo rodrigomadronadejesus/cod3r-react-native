8c43d63db4d7ed115d3c16ed11607d1fd43a68b6
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-06-03 14:16:31 -0300

    Informações de usuário #02

--- tasks/src/screens/Menu.js
+++ tasks/src/screens/Menu.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { StyleSheet, View, Platform, Text } from 'react-native';
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { Gravatar } from 'react-native-gravatar';
+import commonStyles from '../commonStyles';
 
 export default props => {
     return (
         <DrawerContentScrollView>
             <View style={styles.Header}>
+                <Text style={styles.Title}>Tasks</Text>
                 <Gravatar 
                     style={styles.Avatar}
                     options={{
@@ -15,8 +17,8 @@ export default props => {
                     }}
                 />
                 <View style={styles.UserInfo}>
-                    <Text>{props.name}</Text>
-                    <Text>{props.email}</Text>
+                    <Text style={styles.Name}>{props.name}</Text>
+                    <Text style={styles.Email}>{props.email}</Text>
                 </View>
             </View>
             <DrawerItemList {...props} />
@@ -27,16 +29,37 @@ export default props => {
 const styles = StyleSheet.create ({
     Header: {
         borderBottomWidth: 1,
-        borderColor: '#DDD'
+        borderColor: '#DDD',
+        alignItems: 'center'
     },
     Avatar: {
         width: 60,
         height: 60,
         borderWidth: 1,
         borderRadius: 30,
+        backgroundColor: '#222',
         margin: Platform.OS === 'ios' ? 30 : 10
     },
+    Title: {
+        color: '#000',
+        fontFamily: commonStyles.FontFamily,
+        fontSize: 30,
+        paddingTop: Platform.OS === 'ios' ? 50 : 30,
+        padding: 10
+    },
     UserInfo: {
-        
+        marginLeft: 10,
+    },
+    Name: {
+        fontFamily: commonStyles.FontFamily,
+        color: commonStyles.Colors.MainText,
+        fontSize: 20,
+        marginBottom: 5
+    },
+    
+    Email: {
+        fontFamily: commonStyles.FontFamily,
+        fontSize: 15,
+        marginBottom: 5
     }
 });
