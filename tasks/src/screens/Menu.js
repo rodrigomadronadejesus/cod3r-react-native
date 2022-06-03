12162b6f9f799631f42dbf08c410d14ea2c1aa9c
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-06-03 15:02:47 -0300

    Sair da aplicação (Logout)

--- tasks/src/screens/Menu.js
+++ tasks/src/screens/Menu.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { StyleSheet, View, Platform, Text } from 'react-native';
+import { StyleSheet, View, Platform, Text, TouchableOpacity } from 'react-native';
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import { CommonActions } from '@react-navigation/native';
 import { Gravatar } from 'react-native-gravatar';
 import commonStyles from '../commonStyles';
 
+import axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
 export default props => {
+
+    const logout = () => {
+        delete axios.defaults.headers.common['Authorization']
+        AsyncStorage.removeItem('userData')
+        props.navigation.dispatch(
+            CommonActions.reset({
+                index: 0,
+                routes: [
+                    {
+                        name: 'Auth',
+                    },
+                ],
+            })
+        )
+    }
+
     return (
         <DrawerContentScrollView>
             <View style={styles.Header}>
@@ -20,6 +42,11 @@ export default props => {
                     <Text style={styles.Name}>{props.name}</Text>
                     <Text style={styles.Email}>{props.email}</Text>
                 </View>
+                <TouchableOpacity onPress={logout}>
+                    <View style={styles.LogoutIcon}> 
+                        <Icon name='sign-out' size={30} color='#800'/>
+                    </View>
+                </TouchableOpacity>
             </View>
             <DrawerItemList {...props} />
         </DrawerContentScrollView>
@@ -60,5 +87,9 @@ const styles = StyleSheet.create ({
         fontFamily: commonStyles.FontFamily,
         fontSize: 15,
         marginBottom: 5
+    },
+    LogoutIcon: {
+        marginLeft: 10,
+        marginBottom: 10
     }
 });
