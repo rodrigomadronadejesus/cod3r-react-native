b2c57eea776f9d8b951b917944ac36b182dbccd5
Rodrigo Madrona De Jesus <rodrigomadrona@versatecnologia.com.br>
2022-06-03 16:33:06 -0300

    Componente post

--- instagramClone/src/App.js
+++ instagramClone/src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 import Header from './components/Header';
+import Post from './components/Post';
 
 
 export default class App extends Component {
     render(){
         return (
-            <Header/>
+            <View style={{flex: 1}}>
+                <Header/>
+                <Post image={require('./assets/imgs/fence.jpg')}/>
+            </View>
         );
     }
 }
---
+++ instagramClone/src/components/Post.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, Dimensions } from 'react-native';
+
+class Post extends Component {
+    render (){
+        return (
+            <View style={styles.Container}>
+                <Image
+                    source={this.props.image}
+                    style={styles.Image}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create ({
+    Container: {
+        flex: 1
+    },
+    Image: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').width * 3 / 4,
+        resizeMode: 'contain'
+    }
+});
+
+export default Post;
